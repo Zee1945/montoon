04d115e59caeb6f03fb9f393e513009917d82e96
Zul Fachrie <zulfahrie12@gmail.com>
2025-01-05 16:39:24 +0700

    feat: Slicing detail movie

--- resources/js/Components/MovieCard.jsx
+++ resources/js/Components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { Link } from "@inertiajs/react";
 
 
 export default function MovieCard({
@@ -27,6 +28,7 @@ export default function MovieCard({
                                 -translate-x-1/2 z-20 transition ease-in-out duration-500">
                                 <img src="/icons/ic_play.svg" className="" width="50" alt=""/>
                             </div>
-                            <a href={slug} className="inset-0 absolute z-50"></a>
+                            <Link href={route('prototype.movie.show',slug)} className="inset-0 absolute z-50"></Link>
+
                         </div>
 }
